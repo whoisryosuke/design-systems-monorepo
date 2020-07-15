7c80e63259c5b28e4928491a323268fd751bd1a8
Ryosuke <ryosuke.san.hana@gmail.com>
2020-07-14 23:14:32 -0700

    docs(storybook): added TS support for Storybook
    Modified Storybook config to use TS config. NPM deps were added in prev commit.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,17 @@
+const path = require("path");
 module.exports = {
-  stories: ["../packages/**/*.stories.[tj]s"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  stories: ["../packages/**/*.stories.[tj]sx"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    {
+      name: "@storybook/preset-typescript",
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, "../tsconfig.json"),
+        },
+        include: [path.resolve(__dirname)],
+      },
+    },
+  ],
 };
